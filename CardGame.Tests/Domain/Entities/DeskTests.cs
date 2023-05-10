7315ee956bd94245f07c60191855d34dcb98e13a
Cassie Yang <Cassie.Yang@serko.com>
2023-05-10 20:34:19 +1200

    added unit tests for Deck class so to make sure it generated all cards at the beginning

---
+++ CardGame.Tests/Domain/Entities/DeskTests.cs
@@ -0,0 +1,22 @@
+﻿using CardGame.Domain.Entities;
+
+namespace CardGame.Tests.Domain.Entities;
+
+public class DeckTests
+{
+    
+    [Fact]
+    public void ShouldGenerateFiftyTwoCards_WhenInitializingANewDeck()
+    {
+        // Arrange
+        Deck deck = new Deck();
+        int expectedCardCount = 52;
+
+        // Act
+        int actualCardCount = deck.Cards.Count;
+
+        // Assert
+        Assert.Equal(expectedCardCount, actualCardCount);
+    } 
+    
+}
