97ccaea9567695bc39fd00f63e7c94846bc9d6c1
Cassie Yang <Cassie.Yang@serko.com>
2023-05-10 20:43:50 +1200

    Added unit test for the shuffle function of Deck class

--- CardGame.Tests/Domain/Entities/DeskTests.cs
+++ CardGame.Tests/Domain/Entities/DeskTests.cs
@@ -19,4 +19,18 @@ public class DeckTests
         Assert.Equal(expectedCardCount, actualCardCount);
     } 
     
+    [Fact]
+    public void ShouldRandomizeTheCards_WhenShuffleTheDeck()
+    {
+        // Arrange
+        Deck deck = new Deck();
+        Queue<Card> originalCards = new Queue<Card>(deck.Cards);
+
+        // Act
+        deck.Shuffle();
+        Queue<Card> shuffledCards = deck.Cards;
+
+        // Assert
+        Assert.NotEqual(originalCards, shuffledCards);
+    }
 }
