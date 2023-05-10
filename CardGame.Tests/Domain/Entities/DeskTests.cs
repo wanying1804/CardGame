ea4c1f8c426d0983dccbdad30ce2f7af8c4aa0cc
Cassie Yang <Cassie.Yang@serko.com>
2023-05-10 21:17:09 +1200

    added tests for dealCard function so it should return the first card when available and throw exception when no cards

--- CardGame.Tests/Domain/Entities/DeskTests.cs
+++ CardGame.Tests/Domain/Entities/DeskTests.cs
@@ -33,4 +33,37 @@ public class DeckTests
         // Assert
         Assert.NotEqual(originalCards, shuffledCards);
     }
+    
+    [Fact]
+    public void ShouldThrowException_WhenDealCardButNoCardAvailable()
+    {
+        // Arrange
+        Deck deck = new Deck();
+        var cardsTotal = deck.Cards.Count();
+        for (int i = 0; i < cardsTotal; i++)
+        {
+            deck.DealCard();
+        }
+
+        // Assert
+        Assert.Empty( deck.Cards);
+        Assert.Throws<InvalidOperationException>(() => deck.DealCard());
+    }
+    
+    [Fact]
+    public void ShouldReturnTheFirstCard_WhenDealCard()
+    {
+        // Arrange
+        Deck deck = new Deck();
+        var firstCard = deck.Cards.First();
+
+        // Act
+        Card dealtCard = deck.DealCard();
+
+        // Assert
+        Assert.NotNull(dealtCard);
+        Assert.Equal(firstCard, dealtCard);
+    }
+
+
 }
