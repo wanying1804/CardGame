ff0f449632fafc38eab77b8af40f31a3346d91e6
Cassie Yang <Cassie.Yang@serko.com>
2023-05-10 19:48:10 +1200

    added tests for enum of suit

---
+++ CardGame.Tests/Domain/Enums/SuitTests.cs
@@ -0,0 +1,20 @@
+﻿using CardGame.Domain.Enums;
+
+namespace CardGame.Tests.Domain.Enums;
+
+public class SuitTests
+{
+    [Fact]
+    public void ShouldReturnExpectedResult_WhenCompareToTwoSuits()
+    {
+        var spades = Suit.Spades;
+        var hearts = Suit.Hearts;
+        var diamonds = Suit.Diamonds;
+        var clubs = Suit.Clubs;
+        
+        Assert.True(spades > hearts);
+        Assert.True(hearts > diamonds);
+        Assert.True(diamonds > clubs);
+        Assert.True(spades == spades);
+    }
+}
