a773a56fc1fa6547299fafe697472fe9116d3f28
Cassie Yang <Cassie.Yang@serko.com>
2023-05-10 20:17:59 +1200

    added unit test for class Card to make sure the value is between 1 to 13 and comparable

---
+++ CardGame.Tests/Domain/Entities/CardTests.cs
@@ -0,0 +1,26 @@
+﻿using CardGame.Domain.Entities;
+using CardGame.Domain.Enums;
+
+namespace CardGame.Tests.Domain.Entities;
+
+public class CardTests
+{
+    
+    [Fact]
+    public void ShouldThrowsArgumentOutOfRangeException_WhenCardValueIsAboveThirteenOrBelowOne()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Card(Suit.Clubs, 0));
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Card(Suit.Clubs, 14));
+    } 
+   
+    [Fact]
+    public void ShouldReturnExpectedResult_WhenCompareWithTwoCards()
+    {
+        var card1 = new Card (Suit.Spades, 10);
+        var card2 = new Card (Suit.Hearts, 12);
+        var card3 = new Card (Suit.Hearts, 10);
+        
+        Assert.True(card2.CompareTo(card1) > 0);
+        Assert.True(card1.CompareTo(card3) > 0);
+    }
+}
