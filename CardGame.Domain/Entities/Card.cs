db43328fad1c19498da1add14419861987ca54bb
Cassie Yang <Cassie.Yang@serko.com>
2023-05-10 20:31:46 +1200

    updated card class to make sure the value is between 1 to 13 and comparable

--- CardGame.Domain/Entities/Card.cs
+++ CardGame.Domain/Entities/Card.cs
@@ -2,8 +2,39 @@
 
 namespace CardGame.Domain.Entities;
 
-public class Card
+public class Card: IComparable<Card>
 {
-    public Suit Suit { get; set; }
-    public int Value { get; set; }
+    private int _value;
+    public Suit Suit { get;}
+    public int Value
+    {
+        get { return _value; }
+        set
+        {
+            if (value >= 1 && value <= 13)
+                _value = value;
+            else
+                throw new ArgumentOutOfRangeException("Value must be between 1 and 13.");
+        }
+    }
+
+    public Card(Suit suit, int value)
+    {
+        Suit = suit;
+        Value = value;
+    }
+
+    public int CompareTo(Card? other)
+    {
+        // Compare by value first
+        int valueComparison = Value.CompareTo(other?.Value);
+
+        if (valueComparison != 0)
+        {
+            return valueComparison;
+        }
+
+        // If value are the same, compare by suit
+        return Suit.CompareTo(other?.Suit);
+    }
 }
