8ba5b4dfec9874a9e8b527014200fd55c9d7cbf1
Cassie Yang <Cassie.Yang@serko.com>
2023-05-11 13:18:06 +1200

    add DI of application layer to api project

--- CardGame.Api/Program.cs
+++ CardGame.Api/Program.cs
@@ -1,11 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using CardGame.Application;
 
+var builder = WebApplication.CreateBuilder(args);
+{
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builder.Services.AddApplication();
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
 
 var app = builder.Build();
 
---
+++ CardGame.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using CardGame.Application.CardGameService;
+using CardGame.Application.Interfaces;
+using CardGame.Domain.Entities;
+using CardGame.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CardGame.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IGameManager, GameManager>();
+        services.AddScoped<IDeck, Deck>();
+        return services;
+    }
+}
