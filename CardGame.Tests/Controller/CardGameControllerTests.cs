c398e09635074c871c8c68fdf2dc7aa5775d7d9f
Cassie Yang <Cassie.Yang@serko.com>
2023-05-11 14:15:34 +1200

    Added unit test for ResetAndShuffleDeck function in api to make sure we get success response

---
+++ CardGame.Tests/Controller/CardGameControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using CardGame.API.Controllers;
+using CardGame.API.Controllers.Responses;
+using CardGame.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace CardGame.Tests.Controller;
+
+public class CardGameControllerTests
+{
+    private readonly Mock<IGameManager> _gameManagerMock;
+    private readonly GameController _gameController;
+
+    public CardGameControllerTests()
+    {
+        _gameManagerMock = new Mock<IGameManager>();
+        _gameController = new GameController(_gameManagerMock.Object);
+    }
+
+    [Fact]
+    public void ShuffleDeck_ReturnsOkResultWithSuccessMessage()
+    {
+        // Arrange
+        _gameManagerMock.Setup(x => x.ShuffleDeck());
+        _gameManagerMock.Setup(x => x.ResetGame());
+
+        // Act
+        IActionResult result = _gameController.ResetAndShuffleDeck();
+
+        // Assert
+        _gameManagerMock.Verify(d => d.ShuffleDeck(), Times.Once);
+        _gameManagerMock.Verify(d => d.ResetGame(), Times.Once);
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var successMessage = Assert.IsType<SuccessMessage>(okResult.Value);
+        Assert.Equal("Deck reset and shuffled successfully.", successMessage.Message);
+    }
+}
