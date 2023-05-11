27ee00218a8a0e1093f0bdfd0472539656e299e6
Cassie Yang <Cassie.Yang@serko.com>
2023-05-11 22:31:27 +1200

    added PlayRoundResponse

---
+++ CardGame.Api/Controllers/Responses/PlayRoundResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+using CardGame.Domain.Entities;
+using CardGame.Domain.Enums;
+
+namespace CardGame.API.Controllers.Responses;
+public class PlayRoundResponse
+{
+    public IDictionary<PlayerType,Card> Cards { get; }
+    public PlayerType Winner { get; }
+    
+    public PlayRoundResponse(IDictionary<Player, Card> cards, Player? winner)
+    {
+        Cards = new Dictionary<PlayerType, Card>();
+        foreach (var card in cards)
+        {
+            Cards[card.Key.PlayerType] = card.Value;
+        }
+        Winner = winner.PlayerType;
+    }
+
+}
